5bbe7d19296e8590aeece34a1a71a26338079b4c
rohanly <141114696+rohanly@users.noreply.github.com>
2024-03-28 12:11:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { Editor } from "./components/Editor";
+
+function App() {
+  return (
+    <>
+      <Editor />
+    </>
+  );
+}
+
+export default App;
